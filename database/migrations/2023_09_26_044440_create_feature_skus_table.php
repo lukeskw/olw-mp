50bfb968448851e98e7c77fe30311f043eeb132e
lukeskw <lucasporfirioa@gmail.com>
2023-09-26 03:29:38 -0300

    ft: filling models, making relationships, creating factories

--- database/migrations/2023_09_26_044440_create_feature_skus_table.php
+++ database/migrations/2023_09_26_044440_create_feature_skus_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('feature_skus');
+        Schema::dropIfExists('feature_sku');
     }
 };
